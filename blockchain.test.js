e481f1abb1ca5ed44527cdc48c29855275e877f1
Hitvardhan Singh Solanki <Hitvardhan.Solanki@gds.ey.com>
2019-09-10 01:49:34 +0530

    block chain integretity test

--- blockchain.test.js
+++ blockchain.test.js
@@ -2,7 +2,12 @@ const Block = require("./block");
 const Blockchain = require("./blockchain");
 
 describe("Blockchain", () => {
-  const blockchain = new Blockchain();
+  let blockchain;
+
+  beforeEach(() => {
+    blockchain = new Blockchain();
+  });
+
   it("should contain a chain of Array instance", () => {
     expect(blockchain.chain instanceof Array).toBe(true);
   });
@@ -13,5 +18,38 @@ describe("Blockchain", () => {
     const newData = "foo bar";
     blockchain.addBlock({ data: newData });
     expect(blockchain.chain[blockchain.chain.length - 1].data).toEqual(newData);
+  });
+
+  describe("isValidChain()", () => {
+    describe("when chain does not start with the genesis block", () => {
+      it("should return a false", () => {
+        blockchain.chain[0] = { data: "fake-genesis" };
+        expect(blockchain.isValidChain(blockchain.chain)).toBe(false);
+      });
+    });
+    describe("when the chain has a genesis block", () => {
+      beforeEach(() => {
+        blockchain.addBlock({ data: "bears" });
+        blockchain.addBlock({ data: "test" });
+        blockchain.addBlock({ data: "red" });
+      });
+      describe("and a lastHash reference has changed", () => {
+        it("should return false", () => {
+          blockchain.chain[2].lastHash = "broken-chain";
+          expect(blockchain.isValidChain(blockchain.chain)).toBe(false);
+        });
+      });
+      describe("and the chain contains a block with an invalid field", () => {
+        it("should return false", () => {
+          blockchain.chain[2].data = "broken-chain-data";
+          expect(blockchain.isValidChain(blockchain.chain)).toBe(false);
+        });
+      });
+      describe("and the chain is alright", () => {
+        it("returns true", () => {
+          expect(blockchain.isValidChain(blockchain.chain)).toBe(true);
+        });
+      });
+    });
   });
 });
