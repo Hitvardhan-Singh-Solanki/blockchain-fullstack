48156c07a3c26c84df2e8dac7290d2f62e96da0e
Hitvardhan Singh Solanki <Hitvardhan.Solanki@gds.ey.com>
2019-09-11 16:37:11 +0530

    completed blockchain impletementation

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const chalk = require("chalk");
+const request = require("request");
 const Blockchain = require("./blockchain");
 const PubSub = require("./pubsub");
 const bodyParser = require("body-parser");
@@ -7,6 +8,10 @@ const bodyParser = require("body-parser");
 const app = express();
 const blockchain = new Blockchain();
 const pubsub = new PubSub({ blockchain });
+
+const DEFAULT_PORT = 3000;
+
+const ROOT_NODE_ADDRESS = `http://localhost:${DEFAULT_PORT}`;
 
 setTimeout(() => {
   pubsub.broadcastChain();
@@ -20,9 +25,30 @@ app.get("/api/blocks", (_, res) => {
 app.post("/api/mine", (req, res) => {
   const { data } = req.body;
   blockchain.addBlock({ data });
+  pubsub.broadcastChain();
   res.redirect("/api/blocks");
 });
 
-app.listen(3000, () => {
-  console.log(`App is live on PORT: ${chalk.green(3000)}`);
+const syncChain = () => {
+  request({ url: `${ROOT_NODE_ADDRESS}/api/blocks` }, (err, resp, body) => {
+    if (!err && resp.statusCode === 200) {
+      const rootChain = JSON.parse(body);
+      console.log("replace chain on a sync with,", rootChain);
+
+      blockchain.replaceChain(rootChain);
+    }
+  });
+};
+
+let PEER_PORT;
+
+if (process.env.PEER_PORTING === "true") {
+  PEER_PORT = DEFAULT_PORT + Math.ceil(Math.random() * 1000);
+}
+
+const PORT = PEER_PORT || DEFAULT_PORT;
+
+app.listen(PORT, () => {
+  console.log(`App is live on PORT: ${chalk.green(PORT)}`);
+  if (PORT !== DEFAULT_PORT) syncChain();
 });
