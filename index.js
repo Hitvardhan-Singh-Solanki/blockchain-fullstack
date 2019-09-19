62f9588c2979a067cee751094d3502a9b3304bb8
Hitvardhan Singh Solanki <Hitvardhan.Solanki@gds.ey.com>
2019-09-19 22:52:01 +0530

    serving up static files

--- index.js
+++ index.js
@@ -31,6 +31,7 @@ setTimeout(() => {
 }, 1000);
 
 app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, "client")));
 
 app.get("/api/blocks", (_, res) => {
   res.json(blockchain.chain);
