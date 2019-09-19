f793cfa3732b5a9b50110f135792906e3d2de8de
Hitvardhan Singh Solanki <Hitvardhan.Solanki@gds.ey.com>
2019-09-19 23:58:34 +0530

    updating babel

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -1,9 +1,25 @@
 import React, { Component } from "react";
 
 export default class App extends Component {
-  state = {};
+  state = {
+    walletInfo: { address: "", balance: 0 }
+  };
+
+  componentDidMount = async () => {
+    // get wallet info thru fetch
+    const response = await fetch("/api/wallet-info");
+    const jsonResponse = await response.json();
+    this.setState({ walletInfo: jsonResponse });
+  };
 
   render() {
-    return <h1>CLASS BASED</h1>;
+    const { address, balance } = this.state.walletInfo;
+    return (
+      <>
+        <h1>Current wallet</h1>
+        <p>Address: {address}</p>
+        <p>Balance: {balance}</p>
+      </>
+    );
   }
 }
