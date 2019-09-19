cbbb70f690f28ea79d5e3923b7d4e399e0c13fde
Hitvardhan Singh Solanki <Hitvardhan.Solanki@gds.ey.com>
2019-09-19 23:16:28 +0530

    cleaning cache to update live changes

--- client/dist/src.e31bb0bc.js
+++ client/dist/src.e31bb0bc.js
@@ -30666,7 +30666,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53222" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55670" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
